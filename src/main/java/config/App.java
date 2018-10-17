166c05a5e45927f306021a7e2956f6d6e53bdf82
Pepe Valdivia <jvaldivia@softweb.pe>
2018-10-17 07:03:40 -0500

    Decrypt y Encrypt

--- src/main/java/config/App.java
+++ src/main/java/config/App.java
@@ -1,6 +1,28 @@
 package config;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 public class App {
     public static void main(String[] args) {
+        try {
+            EncryptDecrypt ed = new EncryptDecrypt();
+            //encrypt
+            String encrypted = ed.encrypt("kiki123");
+            System.out.println(encrypted);
+            //decrypt
+            String decrypted = ed.decrypt(encrypted);
+            System.out.println(decrypted);
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+        }
     }    
 }
